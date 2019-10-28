9686c8cf1da375703ee1366c12ce7539e6ba3191
Hugo Sobral <hugosobral10@gmail.com>
2019-10-28 11:11:35 -0300

    Modificação nos arquivos de fila em vetor s/ redimensionamento

--- inc/FilaVetor.h
+++ inc/FilaVetor.h
@@ -1,4 +1,17 @@
 #ifndef FILAVETOR_H
 #define FILAVETOR_H
 
+static int N=5;
+int p;
+int u;
+int *fila;
+
+int cria_fila();
+int enfileira(int x);
+int desenfileira(int *pop);
+int fila_cheia();
+int fila_vazia();
+int tamanho_fila();
+void imprime_fila();
+
 #endif
---
+++ src/FilaVetor.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "FilaVetor.h"
+
+
+int cria_fila(){
+    p=0;
+    u=0;
+
+    if (fila==NULL){
+        fila = malloc(N*sizeof(int));
+    } else{
+        free(fila);
+        fila = malloc(N*sizeof(int));
+    }
+
+    if (fila==NULL) return 0;
+    return 1;
+}
+
+int enfileira(int x) {
+    if (fila_cheia()) return 0; 
+
+    fila[u++] = x;
+    return 1;
+}
+
+int desenfileira(int *pop){
+    if (fila_vazia()) return 0;
+
+    *pop = fila[p++];
+    return 1;
+}
+
+int fila_cheia() {
+    return u==N;
+}
+
+int fila_vazia() {
+    return p==u;
+}
+
+int tamanho_fila() {
+    return u-p;
+}
+
+void imprime_fila() {
+    if (fila_vazia()) {
+        printf("\t\t------------------------\n");
+        printf("\t\t| A FILA ESTA VAZIA!!! |\n");
+        printf("\t\t------------------------\n");
+        printf("\n\n");    
+        return;
+    } 
+ 
+    printf("\t\t");
+    for (int i=0; i<N; i++) printf("-------");
+    printf("-\n");
+
+    printf("\t\t");
+    for (int i=0; i<N; i++) {
+        if (i<p || i>=u) printf("| null ");
+        else             printf("| %04d ", fila[i]);
+    } 
+    printf("|\n");
+
+    printf("\t\t");
+    for (int i=0; i<N; i++) printf("-------");
+    printf("-\n");
+
+    printf("\t\t");
+    for (int i=0; i<N; i++) {
+        if (i==p)          printf("   P   ");
+        else if (i==(u-1)) printf("   U   ");
+        else               printf("       ");
+    }
+
+    printf("\n\n");
+    return;
+}

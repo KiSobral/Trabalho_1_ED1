0840392fe3443ccf6b0e85cec243629575b044cc
Hugo Sobral <hugosobral10@gmail.com>
2019-10-31 14:49:15 -0300

    Bugfix

--- src/Main.c
+++ src/Main.c
@@ -3,13 +3,14 @@
 
 /* Espaço reservado para os includes dos módulos das listas */
 // Include da fila com vetor de tamanho fixo
-#include "FilaVetor.h"
+// #include "FilaVetor.h"
 
 // Include da fila circular com redimensionamento
 //#include "FilaCircular.h"
 
 // Include da fila circular com lista encadeada
 #include "FilaLista.h"
+
 
 static int escolha = 0;
 static char pause;
@@ -36,12 +37,12 @@ void Inserir() {
     printf("\n");
 
     if (fila_cheia()) {
-        printf("------------------------\n");
-        printf("| A FILA ESTA CHEIA!!! |\n");
-        printf("------------------------\n");
-        printf("\n\n\n");    
-       
-       Pausa();
+        printf("\t------------------------\n");
+        printf("\t| A FILA ESTA VAZIA!!! |\n");
+        printf("\t------------------------\n");
+        printf("\n\n\n");
+
+        Pausa();
     }
 
     printf("\t=======================================\n");
@@ -95,7 +96,6 @@ void Remover() {
     static int numero_de_remocao;
     static int retorno;
 
-<<<<<<< HEAD
     system("cls || clear");
     printf("\t.______       _______ .___  ___.   ______   ____    ____  _______ .______      \n");
     printf("\t|   _  \\     |   ____||   \\/   |  /  __  \\  \\   \\  /   / |   ____||   _  \\     \n");
@@ -105,137 +105,12 @@ void Remover() {
     printf("\t| _| `._____||_______||__|  |__|  \\______/      \\__/     |_______|| _| `._____|\n");
     printf("\n\n");
 
-
-    if (fila_vazia()) {
+    if (fila_vazia()){
         imprime_fila();
         printf("\n");
 
         Pausa();
     }
-
-    printf("======================================\n");
-    printf("| Elementos disponíveis para remoção |\n");
-    printf("|             De 00 até %02d           |\n", tamanho_fila());
-    printf("======================================\n");
-    printf("Insira o número de elementos a serem removidos: ");
-    scanf("%d", &numero_de_remocao);
-
-    if (numero_de_remocao<0) {
-        printf("======================================\n");
-        printf("|    TAMANHO MINIMO INALCANÇADO!!    |\n");
-        printf("|------------------------------------|\n");
-        printf("| Logo NÃO serão removidos elementos |\n");
-        printf("| da lista.                          |\n");
-        printf("======================================\n");
-        
-        Pausa();
-    } else if (numero_de_remocao>tamanho_fila()){
-        printf("======================================\n");
-        printf("|    TAMANHO MÁXIMO ULTRAPASSADO!!   |\n");
-        printf("|------------------------------------|\n");
-        printf("| Logo serão removidos todos os ele- |\n");
-        printf("| mentos da lista.                   |\n");
-        printf("======================================\n");
-        numero_de_remocao = tamanho_fila();
-    
-    } 
-
-    printf("======================================\n");
-    for (int i=0; i<numero_de_remocao; i++) {
-        desenfileira(&retorno);
-        printf("%dº elemento: %d\n", i+1, retorno);
-    }
-    printf("======================================\n\n\n");
-
-    Pausa();
-}
-
-void Imprimir() {
-    system("cls || clear");
-=======
-static int escolha = 0;
-static char pause;
-
-void Menu();
-
-void Pausa() {
-    printf("Insira algum número para continuar...");
-    scanf("%d", &escolha);
-    Menu();
-}
-
-void Inserir() {
-    static int numero_de_insercao;
-    static int input;
-
-    system("cls || clear");
-    printf("\t __  .__   __.      _______. _______ .______       __  .______      \n");
-    printf("\t|  | |  \\ |  |     /       ||   ____||   _  \\     |  | |   _  \\     \n");
-    printf("\t|  | |   \\|  |    |   (----`|  |__   |  |_)  |    |  | |  |_)  |    \n");
-    printf("\t|  | |  . `  |     \\   \\    |   __|  |      /     |  | |      /     \n");
-    printf("\t|  | |  |\\   | .----)   |   |  |____ |  |\\  \\----.|  | |  |\\  \\----.\n");
-    printf("\t|__| |__| \\__| |_______/    |_______|| _| `._____||__| | _| `._____|\n");
-    printf("\n");
-
-    printf("\t=======================================\n");
-    printf("\t| Elementos disponíveis para inserção |\n");
-    printf("\t|            De 00 até 999            |\n");
-    printf("\t=======================================\n");
-    printf("Insira o número de elementos a serem inseridos: ");
-    scanf("%d", &numero_de_insercao);
-
-    if (numero_de_insercao>999){
-        printf("\t=======================================\n");
-        printf("\t|    TAMANHO MÁXIMO ULTRAPASSADO!!    |\n");
-        printf("\t|-------------------------------------|\n");
-        printf("\t| Logo serão inseridos 999 elementos  |\n");
-        printf("\t| na lista.                           |\n");
-        printf("\t=======================================\n");
-        numero_de_insercao = 999;
-    
-    } else if (numero_de_insercao<0) {
-        printf("\t=======================================\n");
-        printf("\t|    TAMANHO MINIMO INALCANÇADO!!     |\n");
-        printf("\t|-------------------------------------|\n");
-        printf("\t| Logo NÃO serão inseridos elementos  |\n");
-        printf("\t| na lista.                           |\n");
-        printf("\t=======================================\n");
-        
-        Pausa();
-    }
-    
-    printf("\t=======================================\n");
-    for (int i=0; i<numero_de_insercao; i++){
-        printf("\tInsira qual elemento você deseja inserir: ");
-        scanf("%d", &input);
-        if (!enfileira(input)){
-            printf("\t=======================================\n");
-            printf("\t| TAMANHO MAXIMO DA FILA ULTRAPASSADO |\n");
-            printf("\t|-------------------------------------|\n");
-            printf("\t| Foram inseridos %02d elementos na fi- |\n", i);
-            printf("\t| e não foram inseridos %02d elementos  |\n", numero_de_insercao-i);
-            printf("\t=======================================\n\n\n");
-            break;
-        }
-        printf("\t=======================================\n");
-    }
-
-    printf("\n\n");
-    Pausa();
-}
-
-void Remover() {
-    static int numero_de_remocao;
-    static int retorno;
-
-    system("cls || clear");
-    printf("\t.______       _______ .___  ___.   ______   ____    ____  _______ .______      \n");
-    printf("\t|   _  \\     |   ____||   \\/   |  /  __  \\  \\   \\  /   / |   ____||   _  \\     \n");
-    printf("\t|  |_)  |    |  |__   |  \\  /  | |  |  |  |  \\   \\/   /  |  |__   |  |_)  |    \n");
-    printf("\t|      /     |   __|  |  |\\/|  | |  |  |  |   \\      /   |   __|  |      /     \n");
-    printf("\t|  |\\  \\----.|  |____ |  |  |  | |  `--'  |    \\    /    |  |____ |  |\\  \\----.\n");
-    printf("\t| _| `._____||_______||__|  |__|  \\______/      \\__/     |_______|| _| `._____|\n");
-    printf("\n\n");
 
     printf("======================================\n");
     printf("| Elementos disponíveis para remoção |\n");
@@ -276,7 +151,6 @@ void Remover() {
 
 void Imprimir() {
     system("cls || clear");
->>>>>>> 254965cc9fcf769a9cff3b39e783588f002ca91a
     printf("\t __  .___  ___. .______   .______       __  .___  ___.  __  .______      \n");
     printf("\t|  | |   \\/   | |   _  \\  |   _  \\     |  | |   \\/   | |  | |   _  \\     \n");
     printf("\t|  | |  \\  /  | |  |_)  | |  |_)  |    |  | |  \\  /  | |  | |  |_)  |    \n");
@@ -328,6 +202,7 @@ void Sair() {
     printf("\t| Muito Obrigadx por usar este programa. |\n");
     printf("\t==========================================\n\n\n");
 
+    exit(0);
 }
 
 void Menu() {

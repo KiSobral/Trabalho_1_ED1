3309a58beeb28308d290b7220216982e2b246228
Hugo Sobral <hugosobral10@gmail.com>
2019-10-31 03:26:18 -0300

    Implementação do modulo main e atualização do README

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # Trabalho_1_ED1
 Repositório para o versionamento do trabalho 1 da disciplina de Estrutura de Dados 1 (FGA)
+
+___
+## Informações sobre os alunos
+
+| Nome | Matrícula |
+| --- | --- | 
+| Hugo Sobral de Lima Salomão | 18/0018604 |
+| Leonardo Da Silva Gomes     | 18/0021974 |
+
+___
+## Informações acerca da compilação do projeto
+
+Para este projeto, foi pensado na automação dos comandos de compilação através de um makefile.  
+O makefile, então, serviu para que o código seja executado de maneiras distintas de acordo com a necessidade em questão.
+
+### Para compilar todos os códigos fontes, execute:
+```
+make
+```
+Com este comando é gerado um arquivo "**.o**", na pasta **obj/**, para cada arquivo "**.c**", localizados na pasta **src/**.
+
+### Para criar um executável com a fila de vetor
+```
+make vetor
+```
+Com este comando é gerado; a partir dos arquivos "**.o**" dos módulos *main* e *FilaVetor*; um executável  chamado de "**prog**", localizado na pasta **bin/**.
+
+### Para criar um executável com a fila circular
+```
+make circular
+```
+Com este comando é gerado; a partir dos arquivos "**.o**" dos módulos *main* e *FilaCircular*; um executável  chamado de "**prog**", localizado na pasta **bin/**.
+
+### Para criar um executável com a fila circular de lista encadeada
+```
+make lista
+```
+Com este comando é gerado; a partir dos arquivos "**.o**" dos módulos *main* e *FilaLista*; um executável  chamado de "**prog**", localizado na pasta **bin/**.
+
+### Para executar o código
+```
+make run
+```
+Este comando fica responsável por executar o arquivo **prog**.
+
+### Para limpar os executáveis
+```
+make clean
+```
+Com este comando todos os arquivos "**.o**" e o arquivo **prog** serão removidos.
+
+___
+## Para nível de acompanhamento do progresso do trabalho
+Segue o [link do github](https://github.com/KiSobral/Trabalho_1_ED1)
+
+> O repositório só vira a se tornar público assim que o prazo de entrega desta tarefa seja encerrado.
--- src/Main.c
+++ src/Main.c
@@ -9,10 +9,288 @@
 //#include "FilaCircular.h"
 
 // Include da fila circular com lista encadeada
-//#include "FilaLista.h"
+#include "FilaLista.h"
+
+static int escolha = 0;
+static char pause;
+
+void Menu();
+
+void Pausa() {
+    printf("Insira algum número para continuar...");
+    scanf("%d", &escolha);
+    Menu();
+}
+
+void Inserir() {
+    static int numero_de_insercao;
+    static int input;
+
+    system("cls || clear");
+    printf("\t __  .__   __.      _______. _______ .______       __  .______      \n");
+    printf("\t|  | |  \\ |  |     /       ||   ____||   _  \\     |  | |   _  \\     \n");
+    printf("\t|  | |   \\|  |    |   (----`|  |__   |  |_)  |    |  | |  |_)  |    \n");
+    printf("\t|  | |  . `  |     \\   \\    |   __|  |      /     |  | |      /     \n");
+    printf("\t|  | |  |\\   | .----)   |   |  |____ |  |\\  \\----.|  | |  |\\  \\----.\n");
+    printf("\t|__| |__| \\__| |_______/    |_______|| _| `._____||__| | _| `._____|\n");
+    printf("\n");
+
+    if (fila_cheia()) {
+        printf("------------------------\n");
+        printf("| A FILA ESTA CHEIA!!! |\n");
+        printf("------------------------\n");
+        printf("\n\n\n");    
+       
+       Pausa();
+    }
+
+    printf("\t=======================================\n");
+    printf("\t| Elementos disponíveis para inserção |\n");
+    printf("\t|            De 00 até 999            |\n");
+    printf("\t=======================================\n");
+    printf("Insira o número de elementos a serem inseridos: ");
+    scanf("%d", &numero_de_insercao);
+
+    if (numero_de_insercao>999){
+        printf("\t=======================================\n");
+        printf("\t|    TAMANHO MÁXIMO ULTRAPASSADO!!    |\n");
+        printf("\t|-------------------------------------|\n");
+        printf("\t| Logo serão inseridos 999 elementos  |\n");
+        printf("\t| na lista.                           |\n");
+        printf("\t=======================================\n");
+        numero_de_insercao = 999;
+    
+    } else if (numero_de_insercao<0) {
+        printf("\t=======================================\n");
+        printf("\t|    TAMANHO MINIMO INALCANÇADO!!     |\n");
+        printf("\t|-------------------------------------|\n");
+        printf("\t| Logo NÃO serão inseridos elementos  |\n");
+        printf("\t| na lista.                           |\n");
+        printf("\t=======================================\n");
+        
+        Pausa();
+    }
+    
+    printf("\t=======================================\n");
+    for (int i=0; i<numero_de_insercao; i++){
+        printf("\tInsira qual elemento você deseja inserir: ");
+        scanf("%d", &input);
+        if (!enfileira(input)){
+            printf("\t=======================================\n");
+            printf("\t| TAMANHO MAXIMO DA FILA ULTRAPASSADO |\n");
+            printf("\t|-------------------------------------|\n");
+            printf("\t| Foram inseridos %02d elementos na fi- |\n", i);
+            printf("\t| e não foram inseridos %02d elementos  |\n", numero_de_insercao-i);
+            printf("\t=======================================\n\n\n");
+            break;
+        }
+        printf("\t=======================================\n");
+    }
+
+    printf("\n\n");
+    Pausa();
+}
+
+void Remover() {
+    static int numero_de_remocao;
+    static int retorno;
+
+    system("cls || clear");
+    printf("\t.______       _______ .___  ___.   ______   ____    ____  _______ .______      \n");
+    printf("\t|   _  \\     |   ____||   \\/   |  /  __  \\  \\   \\  /   / |   ____||   _  \\     \n");
+    printf("\t|  |_)  |    |  |__   |  \\  /  | |  |  |  |  \\   \\/   /  |  |__   |  |_)  |    \n");
+    printf("\t|      /     |   __|  |  |\\/|  | |  |  |  |   \\      /   |   __|  |      /     \n");
+    printf("\t|  |\\  \\----.|  |____ |  |  |  | |  `--'  |    \\    /    |  |____ |  |\\  \\----.\n");
+    printf("\t| _| `._____||_______||__|  |__|  \\______/      \\__/     |_______|| _| `._____|\n");
+    printf("\n\n");
+
+
+    if (fila_vazia()) {
+        imprime_fila();
+        printf("\n");
+
+        Pausa();
+    }
+
+    printf("======================================\n");
+    printf("| Elementos disponíveis para remoção |\n");
+    printf("|             De 00 até %02d           |\n", tamanho_fila());
+    printf("======================================\n");
+    printf("Insira o número de elementos a serem removidos: ");
+    scanf("%d", &numero_de_remocao);
+
+    if (numero_de_remocao<0) {
+        printf("======================================\n");
+        printf("|    TAMANHO MINIMO INALCANÇADO!!    |\n");
+        printf("|------------------------------------|\n");
+        printf("| Logo NÃO serão removidos elementos |\n");
+        printf("| da lista.                          |\n");
+        printf("======================================\n");
+        
+        Pausa();
+    } else if (numero_de_remocao>tamanho_fila()){
+        printf("======================================\n");
+        printf("|    TAMANHO MÁXIMO ULTRAPASSADO!!   |\n");
+        printf("|------------------------------------|\n");
+        printf("| Logo serão removidos todos os ele- |\n");
+        printf("| mentos da lista.                   |\n");
+        printf("======================================\n");
+        numero_de_remocao = tamanho_fila();
+    
+    } 
+
+    printf("======================================\n");
+    for (int i=0; i<numero_de_remocao; i++) {
+        desenfileira(&retorno);
+        printf("%dº elemento: %d\n", i+1, retorno);
+    }
+    printf("======================================\n\n\n");
+
+    Pausa();
+}
+
+void Imprimir() {
+    system("cls || clear");
+    printf("\t __  .___  ___. .______   .______       __  .___  ___.  __  .______      \n");
+    printf("\t|  | |   \\/   | |   _  \\  |   _  \\     |  | |   \\/   | |  | |   _  \\     \n");
+    printf("\t|  | |  \\  /  | |  |_)  | |  |_)  |    |  | |  \\  /  | |  | |  |_)  |    \n");
+    printf("\t|  | |  |\\/|  | |   ___/  |      /     |  | |  |\\/|  | |  | |      /     \n");
+    printf("\t|  | |  |  |  | |  |      |  |\\  \\----.|  | |  |  |  | |  | |  |\\  \\----.\n");
+    printf("\t|__| |__|  |__| | _|      | _| `._____||__| |__|  |__| |__| | _| `._____|\n");
+    printf("\n\n");
+
+    imprime_fila();
+    printf("\n");
+
+    Pausa();
+}
+
+void Reiniciar() {
+    system("cls || clear");
+    printf("\t.______       _______  __  .__   __.  __    ______  __       ___      .______      \n");
+    printf("\t|   _  \\     |   ____||  | |  \\ |  | |  |  /      ||  |     /   \\     |   _  \\     \n");
+    printf("\t|  |_)  |    |  |__   |  | |   \\|  | |  | |  ,----'|  |    /  ^  \\    |  |_)  |    \n");
+    printf("\t|      /     |   __|  |  | |  . `  | |  | |  |     |  |   /  /_\\  \\   |      /     \n");
+    printf("\t|  |\\  \\----.|  |____ |  | |  |\\   | |  | |  `----.|  |  /  _____  \\  |  |\\  \\----.\n");
+    printf("\t| _| `._____||_______||__| |__| \\__| |__|  \\______||__| /__/     \\__\\ | _| `._____|\n");
+    printf("\n");
+
+    destroi_fila();
+    cria_fila();
+
+    printf("\n\n");
+    printf("\t==========================================\n");
+    printf("\t|         A fila foi reiniciada.         |\n");
+    printf("\t==========================================\n\n\n");
+
+    Pausa();
+}
+
+void Sair() {
+    destroi_fila();
+
+    system("cls || clear");
+    printf("\t.______   ____    ____  _______ \n");
+    printf("\t|   _  \\  \\   \\  /   / |   ____|\n");
+    printf("\t|  |_)  |  \\   \\/   /  |  |__   \n");
+    printf("\t|   _  <    \\_    _/   |   __|  \n");
+    printf("\t|  |_)  |     |  |     |  |____ \n");
+    printf("\t|______/      |__|     |_______|\n");
+    printf("\n");
+
+    printf("\t==========================================\n");
+    printf("\t| Muito Obrigadx por usar este programa. |\n");
+    printf("\t==========================================\n\n\n");
+
+}
+
+void Menu() {
+    system("cls || clear");
+    printf("\t\t.___  ___.  _______ .__   __.  __    __  \n");
+    printf("\t\t|   \\/   | |   ____||  \\ |  | |  |  |  |\n");
+    printf("\t\t|  \\  /  | |  |__   |   \\|  | |  |  |  |\n");
+    printf("\t\t|  |\\/|  | |   __|  |  . `  | |  |  |  |\n");
+    printf("\t\t|  |  |  | |  |____ |  |\\   | |  `--'  |\n");
+    printf("\t\t|__|  |__| |_______||__| \\__|  \\______/ \n");
+    printf("\n");
+    printf("\t\t========================================\n");
+    printf("\t\t|         CENTRAL DE OPÇÕES            |\n");
+    printf("\t\t|--------------------------------------|\n");
+    printf("\t\t|                                      |\n");
+    printf("\t\t| 1. Inserir elementos na fila         |\n");
+    printf("\t\t| 2. Remover elementos da fila         |\n");
+    printf("\t\t| 3. Imprimir a fila                   |\n");
+    printf("\t\t| 4. Reiniciar a fila                  |\n");
+    printf("\t\t| 5. Encerrar o programa               |\n");
+    printf("\t\t|                                      |\n");
+    printf("\t\t========================================\n");
+    printf("\n\n\n");
+
+    printf("Escolha uma opção: ");
+    scanf("%d", &escolha);
+
+    while(escolha<1 || escolha>5) {
+        system("cls || clear");
+        printf("\t\t.___  ___.  _______ .__   __.  __    __  \n");
+        printf("\t\t|   \\/   | |   ____||  \\ |  | |  |  |  |\n");
+        printf("\t\t|  \\  /  | |  |__   |   \\|  | |  |  |  |\n");
+        printf("\t\t|  |\\/|  | |   __|  |  . `  | |  |  |  |\n");
+        printf("\t\t|  |  |  | |  |____ |  |\\   | |  `--'  |\n");
+        printf("\t\t|__|  |__| |_______||__| \\__|  \\______/ \n");
+        printf("\n");
+        printf("\t\t========================================\n");
+        printf("\t\t|         CENTRAL DE OPÇÕES            |\n");
+        printf("\t\t|--------------------------------------|\n");
+        printf("\t\t|                                      |\n");
+        printf("\t\t| 1. Inserir elementos na fila         |\n");
+        printf("\t\t| 2. Remover elementos da fila         |\n");
+        printf("\t\t| 3. Imprimir a fila                   |\n");
+        printf("\t\t| 4. Reiniciar a fila                  |\n");
+        printf("\t\t| 5. Encerrar o programa               |\n");
+        printf("\t\t|                                      |\n");
+        printf("\t\t========================================\n");
+        printf("\n\n\n");
+
+        printf(" --------------------- \n");
+        printf("| OPÇÃO %d INVÁLIDA!! |\n", escolha);
+        printf(" --------------------- \n");
+        printf("Tente novamente: ");
+        scanf("%d", &escolha);
+    }
+
+    switch(escolha) {
+        case 1:
+            Inserir();
+        break;
+        
+        case 2:
+            Remover();
+        break;
+        
+        case 3:
+            Imprimir();
+        break;
+
+        case 4:
+            Reiniciar();
+        break;
+        
+        case 5:
+            Sair();
+        break;
+
+        default:
+            printf("Opção secreta que nunca (provavelmente) vai acontecer!\n");
+            printf("Conspirações do mal e golpes de estado estão armazenados aqui!\n");
+            printf("MUAHAHAHAHAHAHAHA\n");
+            printf("Eh meme :)\n");
+        break;
+    }
+}
 
 int main(){
-    system("cls || clear");
+    cria_fila();
+    Menu();
 
     return 0;
 }
